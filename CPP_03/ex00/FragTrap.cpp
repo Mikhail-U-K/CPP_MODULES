86b9f86f0561221ed8595dd6f74c20d3c4684ad5
Mikhail-U-K <programming2020>
2021-06-08 04:02:42 +0300

    learning inheritance step 1

--- CPP_03/ex00/FragTrap.cpp
+++ CPP_03/ex00/FragTrap.cpp
@@ -27,24 +27,60 @@ FragTrap &FragTrap::operator = (const FragTrap &fragTrap) {
 }
 
 void FragTrap::rangedAttack(const std::string &target) {
-	std::cout << "FR4G-TP <" << Name << "> attacks <" << target << "> at range, causing " << getRangedAttackDmg()<< "points of damage!" << std::endl;
+	std::cout << "*FR4G-TP <" << Name << "> attacks <" << target << "> at range, causing " << getRangedAttackDmg()<< " points of damage!*" << std::endl;
 }
 
-
 void FragTrap::meleeAttack(const std::string &target) {
-
+	std::cout << "*FR4G-TP <" << Name << "> attacks <" << target << "> in melee, causing " << getMeleeAttackDmg()<< " points of damage!*" << std::endl;
 }
 
 void FragTrap::takeDamage(unsigned int amount) {
+	int		cur_hp;
 
+	if(getArmorReduction() > amount)
+		cur_hp = getHp();
+	else
+		cur_hp = getHp() - (amount - getArmorReduction());
+	if (cur_hp < 0)
+		cur_hp = 0;
+	setHp(cur_hp);
+	std::cout << "Why do I even feel pain?!" << std::endl;
 }
 
 void FragTrap::beRepaired(unsigned int amount) {
+	int		cur_hp;
 
+	cur_hp = getHp() + amount;
+	if (cur_hp > getMaxHp())
+		cur_hp = getMaxHp();
+	setHp(cur_hp);
+	std::cout << "Can I just say... yeehaw" << std::endl;
 }
 
 void FragTrap::vaulthunter_dot_exe(const std::string &target) {
+	const int				quantity = 5;
+	std::string		funny_phrases[quantity] = {"It's time for my free grenade giveaway!",
+										"Kill, reload! Kill, reload! KILL! RELOAD!",
+										"Laaasers!",
+										"I feel a joke about poop decks coming on!",
+										"I am rubber, and you are so dead!"};
+	int				i;
+	const int		energy_wasted = 25;
 
+	i = rand() % quantity;
+	if (getEnergyPoint() == 0)
+		std::cout << "Hnngh! Empty!" << std::endl;
+	else
+	{
+		std::cout << funny_phrases[i] << std::endl;
+		if (rand() % 2)
+			rangedAttack(target);
+		else
+			meleeAttack(target);
+		setEnergyPoint(getEnergyPoint() - energy_wasted);
+		if (getEnergyPoint() < 0)
+			setEnergyPoint(0);
+	}
 }
 
 //================================SETTERS==============================//
--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -1,14 +1,17 @@
-//
-// Created by Blinky Daedric on 6/4/21.
-//
-
 #include "FragTrap.hpp"
 
 int main()
 {
-	FragTrap	_robot_1("ClapTrap");
-	FragTrap	_robot_2("ABOBA");
+	FragTrap	model_1("Custodians");
+	FragTrap	model_2("Ferrus Manus");
+	model_1.vaulthunter_dot_exe("Horus");
+	model_1.takeDamage(10);
+	model_1.vaulthunter_dot_exe("Horus");
+	model_1.vaulthunter_dot_exe("Horus");
+	model_1.beRepaired(15);
+	model_1.vaulthunter_dot_exe("Horus");
+	model_1.vaulthunter_dot_exe("Horus");
 
-	_robot_1.rangedAttack(_robot_2.getName());
+//	model_2 = model_1;
 	return (0);
 }
